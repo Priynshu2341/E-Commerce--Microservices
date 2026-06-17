4825624b36f8dab0881148fe50e2ac9c089d5d50
Priyanshu Kushwaha <priyanshukushwaha26@gmail.com>
2026-06-17 21:33:51 +0530

    made project multi-moduled and dockorized all microservices

--- stocks/src/main/java/com/example/stocks/dto/CategoryRequest.java
+++ stocks/src/main/java/com/example/stocks/dto/CategoryRequest.java
@@ -4,9 +4,6 @@ package com.example.stocks.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record CategoryRequest(
-        @NotNull(message = "Stock id is Mandatory")
-        Integer id,
-
         String name,
         String description,
         @NotNull(message = "Stock Quantity is required")
